5b46952feef59633198f33fb89a5392aae06de78
leandrosardi <leandro.sardi@expandedventure.com>
2022-06-27 14:02:23 -0700

    working out the command

--- export.rb
+++ export.rb
@@ -18,4 +18,40 @@ DB = Sequel.odbc('euler') #connect(connection_descriptor)
 puts DB["select db_name() as dbname"].first[:dbname]
 
 
+def profile_descriptor(pid)
+    ret = {}
 
+    p = DB["SELECT name, headline FROM [profile] WHERE [id]='#{pid}'"].first
+    ret[:name] = p[:name]
+    ret[:headline] = p[:headline]
+
+    DB["
+        SELECT name, 
+        FROM [append] 
+        WHERE export_end_time IS NULL
+    "].all { |row|
+        :name => 'Leandro Sardi',
+        :position => 'Founder and CEO',
+        :company => {
+            :name => "ConnectionSphere",
+            :url => "https://connectionsphere.com",
+        },
+        :industry => "Internet",
+        :location => "Argentina",
+        :datas => [
+            {
+                :type => 10,
+                :value => "+54 9 11 5555-5555",
+            },
+            {
+                :type => 20,
+                :value => "support@expandedventure.com",
+            },
+        ],
+end # def profile_descriptor
+
+DB["SELECT TOP 1 id, id_profile FROM [append] WITH (NOLOCK) WHERE export_end_time IS NULL"].all { |row|
+    print "#{row[:id]}... "
+    
+    puts 'done'
+}
